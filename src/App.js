b18413398dbdfc7caba3af8f88ab15fde3497cc0
Eduardo Migueis <edmigueis@hotmail.com>
2024-10-06 16:51:40 -0300

    Ajustes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Main from './pages/Main';
 import MissionPage from './pages/MissionPage';
 import DifficultyPage from './pages/DifficultyPage';
 import './styles/App.css';
-import missions from './missions'; // Import the missions dataset
+import getMissionsByDifficulty from './missions'; // Caminho correto para importar
 
 function App() {
+  const missionPageRef = useRef(); // Create a ref for the MissionPage
+  const [difficulty, setDifficulty] = useState('EASY'); // Default difficulty
+
+  // UseEffect to get the selected difficulty from localStorage
+  useEffect(() => {
+    const savedDifficulty = localStorage.getItem('selectedDifficulty') || 'EASY';
+    setDifficulty(savedDifficulty);
+  }, []);
+
+  // Get missions based on the selected difficulty
+  const missions = getMissionsByDifficulty(difficulty);
+
   return (
     <Router>
       <Routes>
@@ -15,10 +27,16 @@ function App() {
         {missions.map((mission) => (
           <Route
             key={mission.index}
-            path={`/mission-${mission.index + 1}`}
+            path={`/mission-${mission.index}`}
             element={
               <MissionPage
-                stages={mission.stages}
+                ref={missionPageRef} // Pass the ref to MissionPage
+                stages={mission.stages.map((stage) => ({
+                  ...stage,
+                  component: React.cloneElement(stage.component, {
+                    setMissionStageIndex: () => missionPageRef.current.setStageIndex(stage.next),
+                  }),
+                }))}
                 csvPath={mission.csvPath}
                 initialViewState={mission.initialViewState}
                 heatmapConfig={mission.heatmapConfig}

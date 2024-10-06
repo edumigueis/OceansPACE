ea4d4b39bb97cb5be23ded7086891281cc514820
Endrew Bortoli <endrew.ryan977@gmail.com>
2024-10-06 16:36:46 -0300

    Scale return

--- src/pages/MissionPage.js
+++ src/pages/MissionPage.js
@@ -29,6 +29,47 @@ const tileLayerConfig = {
   tileSize: 256,
 };
 
+function HeatmapGradientLegend() {
+  const valueRange = ['0.01', '0.02', '0.05', '0.1', '0.2', '0.5', '1', '2', '5', '10', '20'];
+
+  return (
+    <div style={{
+      position: 'absolute',
+      bottom: 10,
+      right: 10,
+      backgroundColor: 'rgba(255, 255, 255, 0.8)',
+      padding: '10px',
+      borderRadius: '5px',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      zIndex: 1,
+    }}>
+      <div style={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        width: '100%',
+        marginBottom: '5px',
+      }}>
+        {valueRange.map((value, index) => (
+          <span key={index} style={{ fontSize: '8px' }}>{value}</span>
+        ))}
+      </div>
+      <div style={{
+        width: '200px',
+        height: '20px',
+        background: 'linear-gradient(to right, red, yellow, green, cyan, blue)',
+        border: '1px solid #ccc',
+      }}>
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'space-between', width: '100%', marginTop: '5px' }}>
+        <span>Baixa</span>
+        <span>Alta</span>
+      </div>
+    </div>
+  );
+}
+
 function MissionPage({ stages, csvPath }) {
   const [stageIndex, setStageIndex] = useState(0);
   const mapRef = useRef(null);
@@ -49,18 +90,21 @@ function MissionPage({ stages, csvPath }) {
       <div style={{ position: 'relative', display: 'flex', height: '100vh', width: '100vw' }}>
         {currentStage}
         {stages[stageIndex].displayMap ? (
-            <div></div>
-          ) : <span></span>
-          }
+          <div></div>
+        ) : <span></span>
+        }
         <div style={{ flex: 1, zIndex: 0 }}>
           {stages[stageIndex].displayMap ? (
-            <FlatMap
-              ref={mapRef}
-              csvUrl={csvPath}
-              initialViewState={initialViewState}
-              heatmapConfig={heatmapConfig}
-              tileLayerConfig={tileLayerConfig}
-            />
+            <>
+              <FlatMap
+                ref={mapRef}
+                csvUrl={csvPath}
+                initialViewState={initialViewState}
+                heatmapConfig={heatmapConfig}
+                tileLayerConfig={tileLayerConfig}
+              />
+              <HeatmapGradientLegend /> {/* Adicionando a legenda da escala de calor */}
+            </>
           ) : (
             <img
               style={{ height: '100vh', width: '100vw', objectFit: 'cover' }}

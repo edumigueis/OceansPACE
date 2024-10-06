54277c8ca64ce1289329296f9ec970eb8f51b213
Eduardo Migueis <edmigueis@hotmail.com>
2024-10-06 20:30:22 -0300

    Minor fixes

--- src/pages/MissionPage.js
+++ src/pages/MissionPage.js
@@ -2,33 +2,6 @@ import React, { useState, useRef } from 'react';
 import FlatMap from '../components/FlatMap';
 import { MapProvider } from '../components/stages/MapProvider';
 
-const initialViewState = {
-  latitude: 22.8,
-  longitude: 60.5,
-  zoom: 5,
-  pitch: 0,
-  bearing: 0,
-};
-
-const heatmapConfig = {
-  intensity: 1,
-  colorRange: [
-    [255, 0, 0, 255],
-    [255, 255, 0, 255],
-    [0, 255, 0, 255],
-    [0, 255, 255, 255],
-    [0, 0, 255, 255],
-  ],
-  threshold: 0.9,
-};
-
-const tileLayerConfig = {
-  data: "https://c.tile.openstreetmap.org/{z}/{x}/{y}.png",
-  minZoom: 0,
-  maxZoom: 19,
-  tileSize: 256,
-};
-
 function HeatmapGradientLegend() {
   const valueRange = ['0.01', '0.02', '0.05', '0.1', '0.2', '0.5', '1', '2', '5', '10', '20'];
 
@@ -70,16 +43,20 @@ function HeatmapGradientLegend() {
   );
 }
 
-function MissionPage({ stages, csvPath }) {
+function MissionPage({
+  stages,
+  csvPath,
+  initialViewState,
+  heatmapConfig,
+  tileLayerConfig,
+}) {
   const [stageIndex, setStageIndex] = useState(0);
   const mapRef = useRef(null);
 
   const focusOnCoordinates = (latitude, longitude, zoomLevel) => {
-    console.log("aloo");
     if (mapRef.current && mapRef.current.focusOnCoordinates) {
       mapRef.current.focusOnCoordinates(latitude, longitude, zoomLevel);
     }
-    console.log("Focused on coordinates");
   };
 
   const currentStage = React.cloneElement(stages[stageIndex].component, {
@@ -88,13 +65,13 @@ function MissionPage({ stages, csvPath }) {
 
   return (
     <MapProvider focusOnCoordinates={focusOnCoordinates}>
-      <div style={{ position: 'relative', display: 'flex', height: '100vh', width: '100vw'}}>
+      <div style={{ position: 'relative', display: 'flex', height: '100vh', width: '100vw' }}>
         {currentStage}
         {stages[stageIndex].displayMap ? (
           <div></div>
         ) : <span></span>
         }
-        <div style={{ flex: 1, zIndex: 0}}>
+        <div style={{ flex: 1, zIndex: 0 }}>
           {stages[stageIndex].displayMap ? (
             <>
               <FlatMap
@@ -113,7 +90,7 @@ function MissionPage({ stages, csvPath }) {
               alt="mission-location"
             />
           )}
-        </div>  
+        </div>
       </div>
     </MapProvider>
   );

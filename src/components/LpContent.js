8b20f7f27cdbc957ad5f5151c6416bea205fbf87
Eduardo Migueis <edmigueis@hotmail.com>
2024-10-06 21:13:51 -0300

    Ajustes

--- src/components/LpContent.js
+++ src/components/LpContent.js
@@ -12,24 +12,9 @@ const LpContent = ({ setDifficulty }) => {
   ];
   const [currentIndex, setCurrentIndex] = useState(1); // Start at "MEDIUM"
 
-  const handleNext = () => {
-    setCurrentIndex((prevIndex) => {
-      const newIndex = (prevIndex + 1) % options.length;
-      const newDifficulty = options[newIndex];
-      setDifficulty(newDifficulty); // Update difficulty in the parent component
-      localStorage.setItem('selectedDifficulty', newDifficulty); // Save to localStorage
-      return newIndex;
-    });
-  };
-
-  const handlePrev = () => {
-    setCurrentIndex((prevIndex) => {
-      const newIndex = (prevIndex - 1 + options.length) % options.length;
-      const newDifficulty = options[newIndex];
-      setDifficulty(newDifficulty); // Update difficulty in the parent component
-      localStorage.setItem('selectedDifficulty', newDifficulty); // Save to localStorage
-      return newIndex;
-    });
+  const handleSelection = (index) => {
+    setCurrentIndex(index);
+    setDifficulty(options[index].label); // Update difficulty in the parent component
   };
 
   return (
